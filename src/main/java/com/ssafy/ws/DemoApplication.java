651b6061c3b7ce821af81b4b22949f90c188969a
LDG12 <person456@naver.com>
2024-05-04 15:46:30 +0900

    feat: test 파일 추가

--- src/main/java/com/ssafy/ws/DemoApplication.java
+++ src/main/java/com/ssafy/ws/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.ssafy.ws;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.ssafy.ws.model.dao")
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/ssafy/ws/DemoApplicationTests.java
+++ src/test/java/com/ssafy/ws/DemoApplicationTests.java
@@ -1,13 +1,39 @@
 package com.ssafy.ws;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+import com.ssafy.ws.model.dto.ChatRoom;
+import com.ssafy.ws.model.service.ChatService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootTest
+@Slf4j
+class DemoApplicationTests {
+	
+	@Autowired
+	SqlSessionTemplate session;
+	
+	@Autowired
+	ChatService service;
+	
 	@Test
 	void contextLoads() {
+		assertNotNull(session);
 	}
-
+	
+	@Test
+	void insertTest() {
+		ChatRoom room = service.createRoom("test2");
+		log.debug("room : {}", room);
+		int result = service.insert(room);
+		log.debug("result : {}", result);
+		assertEquals(1, result);
+	}
 }
